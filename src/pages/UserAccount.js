a6a779d6be86108c4f70fa085009fc1323f3fb67
issue77 <8477@ait.nsw.edu.au>
2022-08-11 10:23:05 +1000

    firestore working

--- src/pages/UserAccount.js
+++ src/pages/UserAccount.js
@@ -1,41 +1,55 @@
 
 import { UserAuth } from '../context/AuthContext';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { db } from '../firebase'
 import '../styles/Account.css'
 import { collection, getDocs, doc, updateDoc, query, where } from 'firebase/firestore'
 import PopUp from '../components/PopUp'
-import{Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import emailjs from '@emailjs/browser'
+import { async } from '@firebase/util';
 
 
 const UserAccount = () => {
   const { user, logOut } = UserAuth()
   const [rides, setRides] = useState([])
   const [id, setId] = useState('')
-  const [costumers, setCostumers] = useState([])
+
+  const [email, setEmail] = useState('')
+  const [comment, setComment] = useState('')
   const [popupActive, setPopupActive] = useState(false)
   const [buttonPopUp, setButtonPopUp] = useState(false)
 
-  const getRides = () => {
-    const ridesCollectionRef = collection(db, "rides")
-    getDocs(ridesCollectionRef)
-      .then(response => {
-        const rid = response.docs.map(doc => ({
-          ...doc.data(),
-          id: doc.id,
-          viewing: false
-        }))
-        setRides(rid)
-      })
-      .catch(error => console.log(error.message))
+  const getRides = async () => {
+
+    const citiesRef = collection(db, "rides");
+
+// Create a query against the collection.
+const q = query(citiesRef, where("uid", "==",user.uid));
+
+const querySnapshot = await getDocs(q)
+const data=[]
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+  
+  let val = doc.data()
+  val.id = user.uid
+  val.viewing = false
+
+  data.push(val)
+});
+setRides(data)
   }
+
+  
   useEffect(() => {
     getRides()
 
   }, [])
   const handleView = id => {
     const rideClone = [...rides]
-    rideClone.forEach(ride => {
+    rideClone.forEach(ride => { 
       if (ride.id === id) {
         ride.viewing = !ride.viewing
       }
@@ -47,6 +61,22 @@ const UserAccount = () => {
   }
 
 
+  const form = useRef()
+
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_kkfmvad', 'template_g7t30p6', form.current, 'uNsEOEO4to8UWzV9r')
+      .then((result) => {
+        console.log(result.text);
+        
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset()
+
+  };
   const handleSignOut = async () => {
     try {
       await logOut()
@@ -77,18 +107,7 @@ const UserAccount = () => {
                 <h3>Date: {ride.when}</h3>
                 <h3>Addional comments: {ride.comment}</h3>
 
-                <Link className='singinbtn'to='/rides'>Request an update</Link>
-                   <button
-                  type="submit"
-                  onClick={() => setButtonPopUp(true)}
-                  className="send-btn">Request cancelation</button>
-                <PopUp
-                  trigger={buttonPopUp}
-                  setTrigger={setButtonPopUp}>
-                  <p>Any changes are subject to not being accepted by the company. Rates can change according to distance and date</p>
-                </PopUp>
               </div>}
-
               <div className="viewBtn">
                 <button className="view-btn" onClick={() => handleView(ride.id)}> View {ride.viewing ? 'less' : 'more'}</button>
               </div>
@@ -99,6 +118,41 @@ const UserAccount = () => {
 
 
 
+
+        <button
+          type="submit"
+          onClick={() => setButtonPopUp(true)}
+          className="cancelation-btn">Request cancelation</button>
+        <PopUp
+          trigger={buttonPopUp}
+          setTrigger={setButtonPopUp}>
+          <p>Any changes are subject to not being accepted by the company. Rates can change according to distance and date</p>
+          <form ref={form}
+        onSubmit={sendMessage}
+        className='canc-form'>
+          <div >
+
+          <label className='label-form-canc'>Please confirm your email</label>
+          <input
+            placeholder="email"
+            value={email}
+            name="email"
+            className='imput-container-canc'
+            onChange={e => setEmail(e.target.value)}
+            required />
+        </div>
+        <div className='ind-form-canc'>
+          <label className='label-form-canc'>Let us know the reason for the request</label>
+          <textarea 
+            name="comments"
+            value={comment}
+            className='textarea-container-canc' 
+            required
+            onChange={e => setComment(e.target.value)}/>
+        </div>
+        <button className="request-btn">Request Cancelation</button>
+        </form>
+        </PopUp>
 
         <button className="signoutBtn" onClick={handleSignOut}>Sign Out</button>
       </div>
